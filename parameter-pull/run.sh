1028ed4148b4ffbeb6b9a0de0d84d5510e4c3d44
pcs1999.chandra shekar pendem <chandupcs1999@gmail.com>
2023-06-12 11:12:24 +0530

    eks

--- parameter-pull/run.sh
+++ parameter-pull/run.sh
@@ -1,1 +1,12 @@
-aws ssm describe-parameters --region us-east-1 --query "Parameters[*].Name" | xargs -n1  |sed -e 's/,//' | grep ${ENV}.${COMPONENT}
+set -e
+
+aws ssm describe-parameters --region us-east-1 --query "Parameters[*].Name" | xargs -n1  |sed -e 's/,//' | grep ${ENV}.${COMPONENT} >/parameter-store/names
+
+rm -f /parameter-store/params
+
+for param in `cat /parameter-store/names`; do
+  VALUE=$(aws ssm get-parameters --region us-east-1 --names $param  --with-decryption --query Parameters[0].Value | sed 's/"//g'  )
+  KEY=$(echo $param | awk -F . '{print $NF}')
+  echo export $KEY=\"$VALUE\" >>/parameter-store/params
+  cat /parameter-store/params
+done

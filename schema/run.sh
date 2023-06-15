78f5fe7dc3dd327b5f1da95d77cfca4f36b317f6
pcs1999.chandra shekar pendem <chandupcs1999@gmail.com>
2023-06-15 14:43:24 +0530

    eks

--- schema/run.sh
+++ schema/run.sh
@@ -1,18 +1,17 @@
 source /parameter-store/params
 mkdir /code
 cd /code
-git clone https://github.com/pcs1999/${COMPONENT}.git .
-if [ $? -ne 0] ; then
+git clone https://github.com/pcs1999/$COMPONENT.git .
+if [ $? -ne 0 ] ; then
   echo clone failed
   exit 1
 fi
 
 cd /code/schema
-if [ $SCHEMA_TYPE == "mongodb" ] ; then
-    curl -L -o  https://truststore.pki.rds.amazonaws.com/us-east-1/us-east-1-bundle.pem
+if [ $SCHEMA_TYPE == "mongodb" ]; then
+  curl -L -o  rds-combined-ca-bundle.pem  https://truststore.pki.rds.amazonaws.com/us-east-1/us-east-1-bundle.pem
 
-    mongo --ssl --host ${DOCDB_ENDPOINT} --sslCAFile /app/rds-combined-ca-bundle.pem --username ${DOCDB_USER} --password ${DOCDB_PASS} < /code/schema/${COMPONENT}.js
-
-elif [ $SCHEMA_TYPE == "mysql" ] ; then
+  mongo --ssl --host ${DOCDB_ENDPOINT} --sslCAFile rds-combined-ca-bundle.pem --username ${DOCDB_USER} --password ${DOCDB_PASS} < /code/schema/$COMPONENT.js
+elif [ $SCHEMA_TYPE == "mysql" ]; then
   mysql -h${DB_HOST} -u${DB_USER} -p${DB_PASS} < /code/schema/${COMPONENT}.sql
 fi
